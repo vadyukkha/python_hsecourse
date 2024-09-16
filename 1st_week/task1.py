4210ad7bf03e147b641d81454103e63a7d238b07
vadim <144333283+vadyukkha@users.noreply.github.com>
2024-09-16 15:05:30 +0300

    daily solution

---
+++ 1st_week/task1.py
@@ -0,0 +1,26 @@
+"""
+leetcode.com/problem-list/string/
+url: https://leetcode.com/problems/find-the-longest-substring-containing-vowels-in-even-counts/description/
+"""
+
+class Solution:
+    def findTheLongestSubstring(self, s: str) -> int:
+        mp = {
+            "a" : 1, #00001
+            "e" : 2, #00010
+            "i" : 4, #00100
+            "o" : 8, #01000
+            "u" : 16 #10000
+        }
+
+        arr = [-1 for x in range(32)]
+
+        mx_len, xr = 0, 0
+
+        for i in range(len(s)):
+            xr = xr ^ mp[s[i]] if (s[i] in mp.keys()) else xr
+            if (arr[xr] == -1 and xr != 0): arr[xr] = i
+            mx_len = max(mx_len, i - arr[xr])
+        return mx_len
+
+        
---
+++ 1st_week/task2.py
@@ -0,0 +1,13 @@
+"""
+leetcode.com/problem-list/string/
+url: https://leetcode.com/problems/minimum-time-difference/description/
+"""
+class Solution:
+    def findMinDifference(self, timePoints: List[str]) -> int:
+        arr = [int(time[:2])*60 + int(time[-2:]) for time in timePoints]
+        arr.sort()
+        min_diff = 10000
+        for i in range(len(arr) - 1):
+            min_diff = min(min_diff, abs(arr[i] - arr[i+1]))
+        min_diff = min(min_diff, 1440 + arr[0] - arr[-1])
+        return min_diff
