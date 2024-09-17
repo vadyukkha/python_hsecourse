c73ce6dcd1d88faf19dd5d780b5b21fda4fb6ca0
vadim <144333283+vadyukkha@users.noreply.github.com>
2024-09-17 19:43:21 +0300

    add solution

---
+++ 1st_week/task3.py
@@ -0,0 +1,9 @@
+"""
+leetcode.com/problem-list/string/
+url: https://leetcode.com/problems/reverse-words-in-a-string/description/
+"""
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        arr = [w.replace(" ", "") for w in s.split()]
+        return " ".join(arr[::-1])
